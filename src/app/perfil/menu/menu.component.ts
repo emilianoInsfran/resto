60e1fa041f97bf5708a1648c102f87108279ac07
DC36064808 <einsfran@cdt.com.ar>
2020-08-19 01:17:16 -0300

    menu

--- src/app/perfil/menu/menu.component.ts
+++ src/app/perfil/menu/menu.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  getCategoria = [
+    {
+      id:1,
+      nombre:'Pizza',
+      descripcion:''
+    },    {
+      id:1,
+      nombre:'Empanada',
+      descripcion:''
+    },    {
+      id:1,
+      nombre:'Estofado',
+      descripcion:''
+    },    {
+      id:1,
+      nombre:'Papas fritas',
+      descripcion:''
+    },    {
+      id:1,
+      nombre:'Milanesa',
+      descripcion:''
+    },   {
+      id:1,
+      nombre:'Hamburguesa',
+      descripcion:''
+    }
+    
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  agregarPlato(categoria){
+    console.log("set cat",categoria);
   }
 
 }
