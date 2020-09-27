430071abab1835d0f5144bde93e19d5ff48a7f29
DC36064808 <einsfran@cdt.com.ar>
2020-09-27 03:23:42 -0300

    estados de pedidos

--- src/app/estado-pedido/estado-pedido.component.ts
+++ src/app/estado-pedido/estado-pedido.component.ts
@@ -13,39 +13,62 @@ export class EstadoPedidoComponent implements OnInit {
 
   constructor(private route:Router, public utils:UtilsService,private simpleModalService:SimpleModalService ) { }
   arrayPedido=[];
+  tokenPedido:number;
+  id:number;
+  setCodigo:boolean=true;
+  isLoading:boolean=false;
   ngOnInit(): void {
     this.utils.setActiveEstado(1);
-    this.getEstadoPedidos();
-    this.goCategoria();
   }
 
   goCategoria(){
-    setTimeout(()=>{
-      this.route.navigate(['categoria'])
-    }, 45000);
+    if(this.utils.getActiveEstado() == 1 ){
+      setTimeout(()=>{
+        this.route.navigate(['categoria'])
+      }, 45000);
+    }
+
   }
-  
+
+  setCodigoService(){
+    this.utils.setActiveEstado(1);
+    this.id = this.tokenPedido;
+    this.getEstadoPedidos();
+  }
+
   getEstadoPedidos(){
-    console.log("estoy en categorias GET");//_id que te genera mongo
+    console.log("estoy en categorias GET", this.id);//_id que te genera mongo
     //let id = this.utils.getIdResto().resto.id_admin//userid
-    let id = 920;
+    let id = this.id;
+    this.isLoading = true;
     this.utils.getConfig(this.utils.urlDev()+'qrToken/'+id)
       .subscribe((data) => {
         //this.showLoading = false;
         console.log("data->",data);
         this.getArrayEstado(data);
+      },
+      err =>{
+        //this.showLoading =false;
+        console.log("ERROR",err);
+        this.utils.setActiveEstado(2);
+        this.showWarning('CÓDIGO INVÁLIDO');
       });
-
-
+  }
+  
+  getArrayEstado(data){
+    if(data.ok){
+      this.goCategoria();
+      this.setCodigo=false;
+      this.arrayPedido = data.pedido; 
       if(this.utils.getActiveEstado() == 1 ){
         setTimeout(()=>{
           this.getEstadoPedidos(); 
         }, 5000);
       }
-  }
-  
-  getArrayEstado(data){
-    this.arrayPedido = data.pedido; 
+    }else{
+      this.utils.setActiveEstado(2);
+      this.showWarning('CÓDIGO INVÁLIDO')
+    }
 
     console.log("this.setMenu",this.arrayPedido);
   }
@@ -56,6 +79,32 @@ export class EstadoPedidoComponent implements OnInit {
 
   ngOnDestroy() {
     this.utils.setActiveEstado(2);
+  }
+
+
+  showWarning(message) {
+    let disposable = this.simpleModalService.addModal(PopupComponent, {
+      title: 'Confirm title',
+      message: message,
+      opciones:'warning',
+    })
+    .subscribe((isConfirmed)=>{
+        //We get modal result
+        console.log('-',isConfirmed);
+        if(isConfirmed) {
+
+        }
+        else {
+            alert('declined');
+        }
+    });
+    //We can close modal calling disposable.unsubscribe();
+    //If modal was not closed manually close it by timeout
+    setTimeout(()=>{
+        disposable.unsubscribe();
+
+        //this.route.navigate(['categoria'])
+    },2000);
   }
 
 }
